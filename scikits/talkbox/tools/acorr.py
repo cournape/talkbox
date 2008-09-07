128f48978fb118263104309db76d99ec46fa3c15
David Cournapeau <cournape@gmail.com>
2008-09-07 17:23:04 +0000

    Support both scalars and sequences in nextpow2.

--- scikits/talkbox/tools/acorr.py
+++ scikits/talkbox/tools/acorr.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.fftpack import fft
+from scipy.fftpack import fft, ifft
 
 def nextpow2(n):
     """Return the next power of 2 such as 2^p >= n.
@@ -10,10 +10,20 @@ def nextpow2(n):
     Infinite and nan are left untouched, negative values are not allowed."""
     if np.any(n < 0):
         raise ValueError("n should be > 0")
-    f, p = np.frexp(n)
-    res = f
-    bet = np.isfinite(f)
-    exa = (f == 0.5)
-    res[bet] = p[bet]
-    res[exa] = p[exa] - 1
-    return res
+
+    if np.isscalar(n):
+        f, p = np.frexp(n)
+        if f == 0.5:
+            return p-1
+        elif np.isfinite(f):
+            return p
+        else:
+            return f
+    else:
+        f, p = np.frexp(n)
+        res = f
+        bet = np.isfinite(f)
+        exa = (f == 0.5)
+        res[bet] = p[bet]
+        res[exa] = p[exa] - 1
+        return res
--- scikits/talkbox/tools/tests/test_basic.py
+++ scikits/talkbox/tools/tests/test_basic.py
@@ -7,4 +7,11 @@ class TestNextpow2(TestCase):
     X = np.array([0, 1, 2, 3, 4, 6, 8, 15, 16, 17, 32, np.nan, np.infty])
     Y = np.array([0., 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, np.nan, np.infty])
     def test_simple(self):
+        assert nextpow2(0) == 0
+        assert nextpow2(1) == 0
+        assert nextpow2(2) == 1
+        assert nextpow2(3) == 2
+        assert nextpow2(4) == 2
+
+    def test_vector(self):
         assert_array_equal(nextpow2(self.X), self.Y)
