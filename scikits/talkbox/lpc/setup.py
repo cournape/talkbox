8bbede19f3c0bd188c001a6160ef963f8f95945b
David Cournapeau <cournape@gmail.com>
2008-09-14 06:36:42 +0000

    Add python wrapper around C levinson implementation.

--- scikits/talkbox/lpc/setup.py
+++ scikits/talkbox/lpc/setup.py
@@ -1,18 +1,17 @@
 from os.path import join
+import numpy as np
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('lpc', parent_package, top_path)
 
-    config.add_library('liblpc', sources=['src/levinson.c'])
-    #config.add_extension('_lpc',
-    #    sources=['src/levinson.c'],
-    #    libraries=libraries,
-    #    include_dirs=include_dirs,
-    #    **blas_info
-    #)
+    config.add_library('clpc', sources=['src/levinson.c'])
 
+    config.add_extension('_lpc', sources=["src/_lpc.c"],
+                         include_dirs=[np.get_include()],
+                         libraries=["clpc"])
     config.add_data_dir('tests')
+
     return config
 
 if __name__ == '__main__':
---
+++ scikits/talkbox/lpc/src/_lpc.c
@@ -0,0 +1,203 @@
+#include <Python.h>
+#include <numpy/arrayobject.h>
+
+#include "levinson.h"
+
+static PyObject *LpcError;
+
+/*
+ * Malloc are not checked !
+ */
+PyObject* array_levinson_1d(PyArrayObject *arr, long order,
+			    PyArrayObject** alpccoeff, 
+			    PyArrayObject **klpccoeff, PyArrayObject **elpc)
+{
+	PyArray_Descr *dtype;
+	double *acoeff, *kcoeff, *tmp;
+	double err;
+	npy_int alpc_size = (order + 1);
+	npy_int klpc_size = order;
+	npy_int elpc_size = 1;
+
+	/* XXX: use data malloc from numpy */
+	acoeff = malloc(sizeof(*acoeff) * (order + 1));
+	if (acoeff == NULL) {
+		return NULL;
+	}
+	kcoeff = malloc(sizeof(*kcoeff) * order);
+	if (kcoeff == NULL) {
+		return NULL;
+	}
+	tmp = malloc(sizeof(*tmp) * order);
+	if (tmp == NULL) {
+		return NULL;
+	}
+
+	levinson((double*)(arr->data), order, acoeff, &err, kcoeff, tmp);
+
+	*alpccoeff = (PyArrayObject*)PyArray_SimpleNewFromData(1, &alpc_size,
+						      PyArray_DOUBLE, acoeff);
+	*klpccoeff = (PyArrayObject*)PyArray_SimpleNewFromData(1, &klpc_size,
+						      NPY_DOUBLE, kcoeff);
+	*elpc = (PyArrayObject*)PyArray_SimpleNewFromData(1, &elpc_size, NPY_DOUBLE,
+						 &err);
+
+	return 0;
+}
+
+int array_levinson_nd(PyArrayObject *arr, long order,
+			    PyArrayObject** alpccoeff, 
+			    PyArrayObject **klpccoeff, PyArrayObject **elpc)
+{
+	double *acoeff, *kcoeff, *tmp;
+	double *err;
+	double *data;
+	npy_int rank;
+	npy_int *alpc_size;
+	npy_int *klpc_size;
+	npy_int *elpc_size;
+	npy_int n, nrepeat;
+	int i;
+
+	rank = PyArray_NDIM(arr);
+	if (rank < 2) {
+		return -1;
+	}
+
+	/* Set dimensions for output array */
+	alpc_size = malloc(sizeof(*alpc_size) * rank);
+	if (alpc_size == NULL) {
+		return -1;
+	}
+	klpc_size = malloc(sizeof(*klpc_size) * rank);
+	if (klpc_size == NULL) {
+		return -1;
+	}
+	elpc_size = malloc(sizeof(*elpc_size) * (rank-1));
+	if (elpc_size == NULL) {
+		return -1;
+	}
+
+	nrepeat = 1;
+	for (i = 0; i < rank - 1; ++i) {
+		nrepeat *= PyArray_DIM(arr, i);
+		alpc_size[i] = PyArray_DIM(arr, i);
+		klpc_size[i] = PyArray_DIM(arr, i);
+		elpc_size[i] = PyArray_DIM(arr, i);
+	}
+	alpc_size[rank-1] = order + 1;
+	klpc_size[rank-1] = order;
+
+	/* XXX: use data malloc from numpy */
+	acoeff = malloc(sizeof(*acoeff) * nrepeat * (order + 1));
+	if (acoeff == NULL) {
+		return -1;
+	}
+	kcoeff = malloc(sizeof(*kcoeff) * nrepeat * order);
+	if (kcoeff == NULL) {
+		return -1;
+	}
+	err = malloc(sizeof(*err) * nrepeat * order);
+	if (err == NULL) {
+		return -1;
+	}
+	tmp = malloc(sizeof(*tmp) * order);
+	if (tmp == NULL) {
+		return -1;
+	}
+
+	*alpccoeff = (PyArrayObject*)PyArray_SimpleNewFromData(rank, alpc_size,
+						      PyArray_DOUBLE, acoeff);
+	*klpccoeff = (PyArrayObject*)PyArray_SimpleNewFromData(rank, klpc_size,
+						      NPY_DOUBLE, kcoeff);
+	*elpc = (PyArrayObject*)PyArray_SimpleNewFromData(rank-1, elpc_size, NPY_DOUBLE,
+						 err);
+
+	data = (double*)arr->data;
+	n = PyArray_DIM(arr, rank-1);
+	for(i = 0; i < nrepeat; ++i) {
+		levinson(data, order, acoeff, err, kcoeff, tmp);
+		data += n;
+		acoeff += order + 1;
+		kcoeff += order;
+		err += 1;
+	}
+	return 0;
+}
+
+PyObject* array_levinson(PyObject* in, long order)
+{
+	npy_intp rank, size, n;
+	PyArrayObject *arr;
+	PyObject *out = NULL;
+	PyArrayObject *alpc, *klpc, *err;
+
+	arr = (PyArrayObject*)PyArray_ContiguousFromObject(in, PyArray_DOUBLE,
+							   1, 0);
+	if (arr == NULL) {
+		return NULL;
+	}
+	
+	size = PyArray_SIZE(arr);
+	if  (size < 1) {
+		return NULL;
+	}
+	
+	rank = PyArray_NDIM(arr);
+	n = PyArray_DIM(arr, rank-1);
+	if (n <= order) {
+		PyErr_SetString(LpcError, "Order has to be < input size");
+		return NULL;
+	}
+
+	switch(rank) {
+		case 1:
+			array_levinson_1d(arr, order, &alpc, &klpc, &err);
+			break;
+		default:
+			/* Iteratively run levinson recursion on last axis */
+			array_levinson_nd(arr, order, &alpc, &klpc, &err);
+			break;
+	}
+
+	out = PyTuple_New(3);
+	Py_INCREF(alpc);
+	PyTuple_SET_ITEM(out, 0, (PyObject*)alpc);
+
+	Py_INCREF(klpc);
+	PyTuple_SET_ITEM(out, 1, (PyObject*)klpc);
+
+	Py_INCREF(err);
+	PyTuple_SET_ITEM(out, 2, (PyObject*)err);
+
+	return out;
+
+fail:
+    Py_DECREF(arr);
+    return NULL;
+}
+
+PyObject* PyArray_Levinson(PyObject* self, PyObject* args)
+{
+	long order;
+	PyObject *in = NULL;
+	PyObject *out = NULL;
+
+	if (!PyArg_ParseTuple(args, "Ol", &in, &order)) {
+		return NULL;
+	}
+
+	out = array_levinson(in, order);
+	
+	return out;
+}
+
+static PyMethodDef lpcmethods[] = {
+	{"levinson", PyArray_Levinson, METH_VARARGS, NULL}
+};
+
+PyMODINIT_FUNC init_lpc(void)
+{ 
+	Py_InitModule("_lpc", lpcmethods);
+	import_array();
+}
