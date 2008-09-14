318c7470b03c0cf509d4f8af55b2074dd60e89cb
David Cournapeau <cournape@gmail.com>
2008-09-14 13:23:42 +0000

    Add autocorrelation test.

--- scikits/talkbox/tools/tests/test_basic.py
+++ scikits/talkbox/tools/tests/test_basic.py
@@ -1,7 +1,7 @@
 import numpy as np
-from numpy.testing import TestCase, assert_array_equal
+from numpy.testing import TestCase, assert_array_equal, assert_array_almost_equal
 
-from scikits.talkbox.tools.acorr import nextpow2
+from scikits.talkbox.tools.acorr import nextpow2, acorr
 
 class TestNextpow2(TestCase):
     X = np.array([0, 1, 2, 3, 4, 6, 8, 15, 16, 17, 32, np.nan, np.infty])
@@ -15,3 +15,14 @@ class TestNextpow2(TestCase):
 
     def test_vector(self):
         assert_array_equal(nextpow2(self.X), self.Y)
+
+class TestAcorr(TestCase):
+    X = np.linspace(1, 11, 11)
+    Y = np.array([11.0000, 32.0000, 62.0000, 100.0000, 145.0000, 196.0000,
+                  252.0000, 312.0000, 375.0000, 440.0000, 506.0000, 440.0000,
+                  375.0000, 312.0000, 252.0000, 196.0000, 145.0000, 100.0000,
+                  62.0000, 32.0000 , 11.0000])
+
+    def test_simple(self):
+        a = acorr(self.X)
+        assert_array_almost_equal(a, self.Y)
