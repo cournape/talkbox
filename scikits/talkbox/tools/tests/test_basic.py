7919682332b5218b4dd67e4e19fd5bc527eff0a0
David Cournapeau <cournape@gmail.com>
2008-09-14 13:24:23 +0000

    acorr function axis argument tested.

--- scikits/talkbox/tools/tests/test_basic.py
+++ scikits/talkbox/tools/tests/test_basic.py
@@ -23,6 +23,25 @@ class TestAcorr(TestCase):
                   375.0000, 312.0000, 252.0000, 196.0000, 145.0000, 100.0000,
                   62.0000, 32.0000 , 11.0000])
 
+    Xm = np.linspace(1, 22, 22).reshape(2, 11)
+    Ym = np.array([[11.,   32.,   62.,  100.,  145.,  196.,  252.,  312.,  375.,
+                    440.,  506.,  440.,  375.,  312.,  252.,  196.,  145.,  100.,
+                    62.,   32.,   11.],
+                   [264.,   538.,   821.,  1112.,  1410.,  1714.,  2023.,  2336.,
+                    2652.,  2970.,  3289.,  2970.,  2652.,  2336.,  2023.,  1714.,
+                    1410.,  1112.,   821.,   538.,   264.]])
+
     def test_simple(self):
+        """Test autocorrelation for a rank 1 array."""
         a = acorr(self.X)
         assert_array_almost_equal(a, self.Y)
+
+    def test_axis0(self):
+        """Test autocorrelation along default axis."""
+        a = acorr(self.Xm)
+        assert_array_almost_equal(a, self.Ym)
+
+    def test_axis1(self):
+        """Test autocorrelation along axis 0."""
+        a = acorr(self.Xm.T, axis=0)
+        assert_array_almost_equal(a, self.Ym.T)
