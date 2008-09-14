06150c89edf81ffa1bfd39f2d08d6c9283143f66
David Cournapeau <cournape@gmail.com>
2008-09-14 16:23:10 +0000

    Add spectral subpackage to top talkbox setup.

--- scikits/talkbox/setup.py
+++ scikits/talkbox/setup.py
@@ -5,6 +5,7 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
     config = Configuration('talkbox', parent_package, top_path)
     config.add_subpackage('linpred')
+    config.add_subpackage('spectral')
     config.add_subpackage('transforms')
     config.add_subpackage('tools')
     return config
