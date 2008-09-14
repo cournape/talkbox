d9d37eddd2a2c79be6f8e2b961af1b720c89f237
David Cournapeau <cournape@gmail.com>
2008-09-14 14:43:45 +0000

    Add public lpc function.

--- scikits/talkbox/lpc/lpc.py
+++ scikits/talkbox/lpc/lpc.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Last Change: Sun Sep 14 10:00 PM 2008 J
+# Last Change: Sun Sep 14 11:00 PM 2008 J
 
 import numpy as np
 from scipy.fftpack import fft, ifft
@@ -10,6 +10,39 @@ from scikits.talkbox.tools import nextpow2
 from _lpc import levinson as c_levinson
 
 __all__ = ['levinson']
+
+def lpc(signal, order, axis=-1):
+    """Compute the Linear Prediction Coefficients.
+
+    Return the order + 1 LPC coefficients for the signal. c = lpc(x, k) will
+    find the k+1 coefficients of a k order linear filter:
+
+      xp[n] = -c[1] * x[n-2] - ... - c[k-1] * x[n-k-1]
+
+    Such as the sum of the squared-error e[i] = xp[i] - x[i] is minimized.
+
+    Parameters
+    ----------
+
+    signal: array_like
+        input signal
+    order : int
+        LPC order (the output will have order + 1 items)
+
+    Note
+    ----
+
+    This uses Levinson-Durbin recursion for the autocorrelation matrix
+    inversion, and fft for the autocorrelation computation. 
+    
+    For small order, particularly if order << signal size, direct computation
+    of the autocorrelation is faster: use levinson and correlate in this case."""
+    n = signal.shape[axis]
+    if order > n:
+        raise ValueError("Input signal must have length >= order")
+
+    r = acorr_lpc(signal, axis)
+    return levinson(r, order, axis)
 
 def _acorr_last_axis(x, nfft, maxlag):
     a = np.real(ifft(np.abs(fft(x, n=nfft) ** 2)))
