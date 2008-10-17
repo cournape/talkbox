08e6650af1f061d9e540df31899456d0adf0554b
David Cournapeau <cournape@gmail.com>
2008-10-17 15:15:00 +0000

    Add comment relative to dyscrepanty between R and talkbox periodogram.

--- scikits/talkbox/spectral/tests/test_basic.py
+++ scikits/talkbox/spectral/tests/test_basic.py
@@ -11,6 +11,7 @@ lh = np.array([2.4, 2.4, 2.4, 2.2, 2.1, 1.5, 2.3, 2.3, 2.5, 2.0, 1.9, 1.7,
 2.1, 3.4, 3.0, 2.9])
 
 # Returned by p = spectrum(lh, plot=FALSE, detrend=FALSE, taper=0.0)
+# XXX: R drops the first item (DC component) of the estimated spectrogram
 lh_spec_raw = np.array([0.32650971, 0.79865114, 1.25684523, 0.66284366,
 0.13803913, 1.51075717, 0.23526771, 0.66520833, 0.28393413, 0.09545270,
 0.24710226, 0.18541667, 0.02353390, 0.07176963, 0.05049468, 0.11312500,
