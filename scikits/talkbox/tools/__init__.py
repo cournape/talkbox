0b7f99bcb4e42c50263a7d8a42513876b02b445a
David Cournapeau <cournape@gmail.com>
2008-12-08 12:32:46 +0000

    Put segment_axis in the main scikits.talkbox namespace.

--- scikits/talkbox/tools/__init__.py
+++ scikits/talkbox/tools/__init__.py
@@ -7,3 +7,6 @@ __all__ += correlations.__all__
 import cffilter
 from cffilter import cslfilter as slfilter
 __all__ += ['slfilter']
+
+from segmentaxis import segment_axis
+__all__ += ['segment_axis']
