9fb5c703069e086eb710f1bf601a046188f07880
David Cournapeau <cournape@gmail.com>
2009-01-18 08:27:39 +0000

    Put filter bank code in its own function for MFCC.

--- scikits/talkbox/features/mfcc.py
+++ scikits/talkbox/features/mfcc.py
@@ -9,6 +9,41 @@ from scikits.talkbox import segment_axis
 
 from mel import hz2mel
 
+def trfbank(fs, nfft, lowfreq, linsc, logsc, nlinfilt, nlogfilt):
+    """Compute triangular filterbank for MFCC computation."""
+    # Total number of filters
+    nfilt = nlinfilt + nlogfilt
+
+    #------------------------
+    # Compute the filter bank
+    #------------------------
+    # Compute start/middle/end points of the triangular filters in spectral
+    # domain
+    freqs = np.zeros(nfilt+2)
+    freqs[:nlinfilt] = lowfreq + np.arange(nlinfilt) * linsc
+    freqs[nlinfilt:] = freqs[nlinfilt-1] * logsc ** np.arange(1, nlogfilt + 3)
+    heights = 2./(freqs[2:] - freqs[0:-2])
+
+    # Compute filterbank coeff (in fft domain, in bins)
+    fbank = np.zeros((nfilt, nfft))
+    # FFT bins (in Hz)
+    nfreqs = np.arange(nfft) / (1. * nfft) * fs
+    for i in range(nfilt):
+        low = freqs[i]
+        cen = freqs[i+1]
+        hi = freqs[i+2]
+
+        lid = np.arange(np.floor(low * nfft / fs) + 1,
+                        np.floor(cen * nfft / fs) + 1, dtype=np.int)
+        lslope = heights[i] / (cen - low)
+        rid = np.arange(np.floor(cen * nfft / fs) + 1,
+                        np.floor(hi * nfft / fs) + 1, dtype=np.int)
+        rslope = heights[i] / (hi - cen)
+        fbank[i][lid] = lslope * (nfreqs[lid] - low)
+        fbank[i][rid] = rslope * (hi - nfreqs[rid])
+
+    return fbank
+
 def mfcc(input, nwin=256, nfft=512, fs=16000, nceps=13):
     # MFCC parameters: taken from auditory toolbox
     over = nwin - 160
@@ -26,32 +61,7 @@ def mfcc(input, nwin=256, nfft=512, fs=16000, nceps=13):
 
     w = hamming(nwin, sym=0)
 
-    #------------------------
-    # Compute the filter bank
-    #------------------------
-    # Compute start/middle/end points of the triangular filters
-    freqs = np.zeros(nfil+2)
-    freqs[:nlinfil] = lowfreq + np.arange(nlinfil) * linsc
-    freqs[nlinfil:] = freqs[nlinfil-1] * logsc ** np.arange(1, nlogfil + 3)
-    heights = 2./(freqs[2:] - freqs[0:-2])
-
-    # Compute filterbank coeff (in fft domain, in bins)
-    fbank = np.zeros((nfil, nfft))
-    # FFT bins (in Hz)
-    nfreqs = np.arange(nfft) / (1. * nfft) * fs
-    for i in range(nfil):
-        low = freqs[i]
-        cen = freqs[i+1]
-        hi = freqs[i+2]
-
-        lid = np.arange(np.floor(low * nfft / fs) + 1,
-                        np.floor(cen * nfft / fs) + 1, dtype=np.int)
-        lslope = heights[i] / (cen - low)
-        rid = np.arange(np.floor(cen * nfft / fs) + 1,
-                        np.floor(hi * nfft / fs) + 1, dtype=np.int)
-        rslope = heights[i] / (hi - cen)
-        fbank[i][lid] = lslope * (nfreqs[lid] - low)
-        fbank[i][rid] = rslope * (hi - nfreqs[rid])
+    fbank = trfbank(fs, nfft, lowfreq, linsc, logsc, nlinfil, nlogfil)
 
     #------------------
     # Compute the MFCC 
