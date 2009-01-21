ce86bc842a31c5f2db742db3c0e7d578ddfb7add
David Cournapeau <cournape@gmail.com>
2009-01-21 11:32:32 +0000

    Add features submodule to the talkbox package.

---
+++ scikits/talkbox/features/__init__.py
@@ -0,0 +1,5 @@
+from scikits.talkbox.features.mel import mel2hz, hz2mel
+__all__  = ['mel2hz', 'hz2mel']
+
+from scikits.talkbox.features.mfcc import mfcc
+__all__ += ['mfcc']
---
+++ scikits/talkbox/features/setup.py
@@ -0,0 +1,12 @@
+from os.path import join
+import numpy as np
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('features', parent_package, top_path)
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
