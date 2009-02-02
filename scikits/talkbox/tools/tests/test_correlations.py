9157214eb5004e5b003734cb0f90137b2ba501dd
David Cournapeau <cournape@gmail.com>
2009-02-02 12:17:44 +0000

    Add tests for scale option of autocorrelation.

--- scikits/talkbox/tools/tests/test_correlations.py
+++ scikits/talkbox/tools/tests/test_correlations.py
@@ -24,6 +24,12 @@ class _TestCorrCommon(TestCase):
                   252.0000, 312.0000, 375.0000, 440.0000, 506.0000, 440.0000,
                   375.0000, 312.0000, 252.0000, 196.0000, 145.0000, 100.0000,
                   62.0000, 32.0000 , 11.0000])
+    Yc = np.array([   0.02173913043478, 0.06324110671937, 0.12252964426877,
+        0.19762845849802, 0.28656126482213, 0.38735177865613, 0.49802371541502,
+        0.61660079051383, 0.74110671936759, 0.86956521739130, 1.00000000000000,
+        0.86956521739130, 0.74110671936759, 0.61660079051383, 0.49802371541502,
+        0.38735177865613, 0.28656126482213, 0.19762845849802, 0.12252964426877,
+        0.06324110671937, 0.02173913043478,])
 
     Xm = np.linspace(1, 22, 22).reshape(2, 11)
     Ym = np.array([[11.,   32.,   62.,  100.,  145.,  196.,  252.,  312.,  375.,
@@ -48,16 +54,60 @@ class _TestCorrCommon(TestCase):
         a = self.acorr(self.Xm.T, axis=0)
         assert_array_almost_equal(a, self.Ym.T)
 
+    def test_normalization(self):
+        a = self.acorr(self.X, scale='coeff')
+        assert_array_almost_equal(a, self.Yc)
+
+    def test_normalization_onesided(self):
+        a = self.acorr(self.X, scale='coeff', onesided=True)
+        assert_array_almost_equal(a, self.Yc[self.X.size-1:])
+
+    def test_normalization_axis1(self):
+        x = np.random.randn(5, 25)
+        a = np.zeros((5, 49))
+        for i in range(5):
+            a[i] = self.acorr(x[i], scale='coeff')
+        b = self.acorr(x, scale='coeff', axis=-1)
+
+        assert_array_almost_equal(b, a)
+
+    def test_normalization_axis0(self):
+        x = np.random.randn(5, 25)
+        a = np.zeros((9, 25))
+        for i in range(25):
+            a[:, i] = self.acorr(x[:, i], scale='coeff', axis=0)
+        b = self.acorr(x, scale='coeff', axis=0)
+
+        assert_array_almost_equal(b, a)
+
+    def test_normalization_onesided_axis1(self):
+        x = np.random.randn(5, 25)
+        a = np.zeros((5, 25))
+        for i in range(5):
+            a[i] = self.acorr(x[i], scale='coeff', onesided=True)
+        b = self.acorr(x, scale='coeff', axis=-1, onesided=True)
+
+        assert_array_almost_equal(b, a)
+
+    def test_normalization_onesided_axis0(self):
+        x = np.random.randn(5, 25)
+        a = np.zeros((5, 25))
+        for i in range(25):
+            a[:, i] = self.acorr(x[:, i], scale='coeff', axis=0, onesided=True)
+        b = self.acorr(x, scale='coeff', axis=0, onesided=True)
+
+        assert_array_almost_equal(b, a)
+
 class TestAcorr(_TestCorrCommon):
     def setUp(self):
         self.acorr = acorr
 
-class TestCythonAcorr(_TestCorrCommon):
-    def setUp(self):
-        self.acorr = cacorr
-        self.X = self.X[np.newaxis, :]
-        self.Y = self.Y[np.newaxis, :]
-
-    @dec.skipif(True, "Arbitrary axis not suppported yet in cython version")
-    def test_axis1(self):
-        pass
+#class TestCythonAcorr(_TestCorrCommon):
+#    def setUp(self):
+#        self.acorr = cacorr
+#        self.X = self.X[np.newaxis, :]
+#        self.Y = self.Y[np.newaxis, :]
+#
+#    @dec.skipif(True, "Arbitrary axis not suppported yet in cython version")
+#    def test_axis1(self):
+#        pass
